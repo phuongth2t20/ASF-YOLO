caf01e3ea7aea1c378f3053f12bc50f2ee88b2c9
phuongth2t20 <76458387+phuongth2t20@users.noreply.github.com>
2024-12-05 22:58:32 +0700

    Update train.py

--- segment/train.py
+++ segment/train.py
@@ -308,7 +308,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                     imgs = nn.functional.interpolate(imgs, size=ns, mode='bilinear', align_corners=False)
 
             # Forward
-            with torch.amp.autocast('cuda', amp):
+            with torch.amp.autocast('cuda', dtype=torch.float16):
                 pred = model(imgs)  # forward
                 loss, loss_items = compute_loss(pred, targets.to(device), masks=masks.to(device).float())
                 if RANK != -1:
